a98622dab679359674629ad2b488fab8c941d710
joehsuLM <joseph.hsu@libertymutual.com>
2017-03-17 16:11:47 -0400

    updated game logic

--- js/main.js
+++ js/main.js
@@ -22,28 +22,41 @@ var cards = [
 	}
 ];
 
+var originalGameBoard = document.getElementById('game-board');
+
 // this is an empty array. once the user selects the cards, they are put into this array.
 var cardsInPlay = [];
 
 // this function checks for a match from the array:cardsInPlay
 var checkForMatch = function () {
 	
-	console.log("cards selected: " + cardsInPlay);
+	console.log("cards selected\ncard 1: " + cardsInPlay[0] + "\ncard 2: " + cardsInPlay[1]);
+	/* this if does not check if the same exact card was selected
 	if (cardsInPlay[0] === cardsInPlay[1]){
 			alert("You found a match!");
 		} else {
 			alert("Sorry, try again");
 	}
+	*/
+
+	// attempt to check if same card is selected. if so, doesn't not qualify as a matchg
+	if (cardsInPlay[0].rank === cardsInPlay[1].rank && cardsInPlay[0].suit === cardsInPlay[1].suit){
+			alert("Don't select the same card twice. Try again!")
+		} else if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again!");
+			//reloadBoard();
+			//createBoard();
+		}
 };
 
 // this function will run output to the console the cards chosen and their attributes
 // addtionally, it will check for a match using the function:checkForMatch
 var flipCard = function () {
 	var cardId = this.getAttribute('data-id')
-	//console.log ("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank);
-	//console.log(cards[cardId].suit);
-	//console.log(cards[cardId].cardImage);
+
+	cardsInPlay.push(cards[cardId]);
 
 	// loads the image of the card face when clicked
 	this.setAttribute('src', cards[cardId].cardImage);
@@ -65,9 +78,21 @@ var createBoard = function () {
 	}
 };
 
+var reloadBoard = function () {
+	gameBoard = document.querySelector('.boardClearFix');
+	//this.parentNode.parentNode.removeChild(this.parentNode);
+	document.querySelector('main').removeChild('div');
+	createBoard();
+};
 
+// hides the gameboard and show contents of instruction
+var showInstructions = function () {
+	//gameBoard = document.querySelector('#game-board');
+	document.querySelector('#game-board').style.display = 'none';
+};
 // calls the function:game-board to create a board wit the cards
 createBoard();
+//showInstructions();
 
 
 
